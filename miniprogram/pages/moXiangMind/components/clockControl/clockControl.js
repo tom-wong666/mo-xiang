28a05d7e1d750a479d9c428501f3f56daa62c1f1
Tom Wong <3766639606@qq.com>
2020-02-14 20:20:29 +0800

    完成专注功能

--- miniprogram/pages/moXiangMind/components/clockControl/clockControl.js
+++ miniprogram/pages/moXiangMind/components/clockControl/clockControl.js
@@ -3,53 +3,140 @@
 wx.setKeepScreenOn({
   keepScreenOn: true
 })
-let time = 15 * 60;
-let timer = 0;
+// 总时长15分钟
+let time = 15 * 60
+// 定时器
+let timer = 0
+// 计时标识
+let isMoving = true
+// 重置时钟标识
+let isRestartClock = false
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    // 剩余时间
     balanceTime: "15:00",
+    // 剩余比例
+    balancePercent: 100,
+    // 计时中，已暂停
+    countStatus: '计时中',
+    // 暂停计时，开始计时
+    controlStatus: '暂停计时'
   },
 
   /**
-   * 页面方法
+   * 获取剩余时间函数
    */
-  methods: {
-    getBalanceTime(time) {
-      let minute = Math.floor(time / 60)
-      minute = this.formatNumber(minute)
-      let second = time % 60
-      second = this.formatNumber(second)
-      return minute + ':' + second
-    },
-    formatNumber(smallNumber) {
-      if (smallNumber < 10) {
-        return '0' + smallNumber.toString()
+  getBalanceTime(time) {
+    let minute = Math.floor(time / 60)
+    minute = this.formatNumber(minute)
+    let second = time % 60
+    second = this.formatNumber(second)
+    return minute + ':' + second
+  },
+
+  /**
+   * 格式化小于10的数字(补0)
+   */
+  formatNumber(smallNumber) {
+    if (smallNumber < 10) {
+      return '0' + smallNumber.toString()
+    } else {
+      return smallNumber.toString()
+    }
+  },
+
+  /**
+   * 开始||暂停 控制
+   */
+  controlTimer() {
+    if (isRestartClock) { 
+      // 初始化数据
+      time = 15 * 60
+      timer = 0
+      isMoving = true
+      // 初始化data
+      this.setData({
+        // 剩余时间
+        balanceTime: "15:00",
+        // 剩余比例
+        balancePercent: 100,
+        // 计时中，已暂停
+        countStatus: '计时中',
+        // 暂停计时，开始计时
+        controlStatus: '暂停计时'
+      })
+      // 开启定时器
+      timer = setInterval(() => {
+        this.startTimer()
+      }, 1000)
+      isRestartClock = false
+    } else {
+      if (isMoving) {
+        this.setData({
+          countStatus: '已暂停',
+          controlStatus: '开始计时'
+        })
+        clearInterval(timer)
+        timer = 0
+        isMoving = false
       } else {
-        return smallNumber.toString()
+        this.setData({
+          countStatus: '计时中',
+          controlStatus: '暂停计时'
+        })
+        timer = setInterval(() => {
+          this.startTimer()
+        }, 1000)
+        isMoving = true
       }
     }
   },
 
   /**
+   * 开始计数函数
+   */
+  startTimer() {
+    if (time > 0) {
+      time = time - 1
+      const balanceTime = this.getBalanceTime(time)
+      const balancePercent = parseFloat(((time / 900) * 100).toFixed(2))
+      this.setData({
+        balanceTime: balanceTime,
+        balancePercent: balancePercent
+      })
+    } else {
+      clearInterval(timer)
+      this.setData({
+        countStatus: '一刻钟过去了',
+        controlStatus: '再来一刻钟'
+      })
+      isRestartClock = true
+    }
+  },
+
+  /**
+   * 返回上一级
+   */
+  backToMind() {
+    console.log(11)
+    wx.switchTab({ url: '/pages/moXiangMind/moXiangMind' })
+  },
+  /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    time = 1 * 10;
-    timer = 0;
+    // 初始化数据
+    time = 15 * 60
+    timer = 0
+    isMoving = true
+    isRestartClock = false
+    // 开启定时器
     timer = setInterval(() => {
-      if (time > 0) {
-        time = time - 1
-        const balanceTime = this.methods.getBalanceTime(time)
-        this.setData({
-          balanceTime: balanceTime
-        })
-      } else {
-        clearInterval(timer)
-      }
+      this.startTimer()
     }, 1000)
   },
 
@@ -78,7 +165,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    console.log('timer', timer)
+    // 清理定时器
     clearInterval(timer)
   },
 
