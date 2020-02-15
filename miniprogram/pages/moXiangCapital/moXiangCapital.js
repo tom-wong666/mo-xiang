a688e803cf05d9d9a3a4432c147b281bd1e746bc
Tom Wong <3766639606@qq.com>
2020-02-15 08:31:14 +0800

    完成字数统计

--- miniprogram/pages/moXiangCapital/moXiangCapital.js
+++ miniprogram/pages/moXiangCapital/moXiangCapital.js
@@ -5,14 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    capitalResult: '零元整'
   },
 
+  /**
+   * 数字转大写函数
+   */
+  numberToChinese(num) {
+    var num = parseFloat(num);
+    var strOutput = "",
+      strUnit = '仟佰拾亿仟佰拾万仟佰拾元角分';
+    num += "00";
+    var intPos = num.indexOf('.');
+    if (intPos >= 0) {
+      num = num.substring(0, intPos) + num.substr(intPos + 1, 2);
+    }
+    strUnit = strUnit.substr(strUnit.length - num.length);
+    for (var i = 0; i < num.length; i++) {
+      strOutput += '零壹贰叁肆伍陆柒捌玖'.substr(num.substr(i, 1), 1) + strUnit.substr(i, 1);
+    }
+    return strOutput.replace(/零角零分$/, '整').replace(/零[仟佰拾]/g, '零').replace(/零{2,}/g, '零').replace(/零([亿|万])/g, '$1').replace(/零+元/, '元').replace(/亿零{0,3}万/, '亿').replace(/^元/, "零元")
+  },
+
+  /**
+   * 监控输入
+   */
+  bindKeyInput: function (e) {
+    const capitalResult = this.numberToChinese(e.detail.value)
+    this.setData({
+      capitalResult: capitalResult
+    })
+    // 2.1.0 起支持，处理函数可以直接 return 一个字符串，将替换输入框的内容
+    // 这里用来放正则
+    // 小数点左边支持12位，右边支持2位
+    // return 123
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
--- miniprogram/pages/moXiangQuantity/moXiangQuantity.js
+++ miniprogram/pages/moXiangQuantity/moXiangQuantity.js
@@ -5,7 +5,50 @@ Page({
    * 页面的初始数据
    */
   data: {
+    stringResult: ''
+  },
 
+  /**
+   * 监控输入
+   */
+  bindKeyInput(e) {
+    const res = this.countResult(e.detail.value)
+    this.setData({
+      stringResult: res
+    })
+  },
+
+  /**
+   * 合并统计结果
+   */
+  countResult(val) {
+    // 定义基础变量
+    const totalLength = '您一共输入了' + val.length + '个字符，其中'
+    const chineseWord = '中文字符'
+    let chineseWordQty = 0
+    const chineseWordPattern = new RegExp("[\u4E00-\u9FA5]+")
+    const englishWord = '英文字符'
+    let englishWordQty = 0
+    const englishWordPattern = new RegExp("[A-Za-z]+")
+    const numberWord = '阿拉伯数字'
+    let numberWordQty = 0
+    const numberWordPattern = new RegExp("[0-9]+")
+    const otherWord = '其他字符'
+    let otherWordQty = 0
+    // 循环处理输入字符
+    const valArray = val.split('')
+    for (const item of valArray) {
+      if(chineseWordPattern.test(item)) {
+        chineseWordQty = chineseWordQty + 1
+      } else if (englishWordPattern.test(item)) {
+        englishWordQty = englishWordQty + 1
+      } else if (numberWordPattern.test(item)) {
+        numberWordQty = numberWordQty + 1
+      } else {
+        otherWordQty = otherWordQty + 1
+      }   
+    }
+    return `${totalLength}${chineseWord}${chineseWordQty}个，${englishWord}${englishWordQty}个，${numberWord}${numberWordQty}个，${otherWord}${otherWordQty}个。`
   },
 
   /**
