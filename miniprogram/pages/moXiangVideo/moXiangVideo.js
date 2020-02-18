b1a23877a0df68f2a009b4dcf26e357e5d21d906
tom-wong666 <jobtao@126.com>
2020-02-18 16:14:28 +0800

    完善视频描述

--- miniprogram/pages/moXiangVideo/moXiangVideo.js
+++ miniprogram/pages/moXiangVideo/moXiangVideo.js
@@ -8,7 +8,7 @@ Page({
     videoList: [
       { id: 1, title: '捉妖记2-坏妖王来袭', type: '影视剪辑', playTime: '00:52', src: 'cloud://mo-xiang-wx-yun.6d6f-mo-xiang-wx-yun-1301177803/source/video/捉妖记2-坏妖王来袭.mp4', detail: '胡巴和小妖们载歌载舞时，坏妖王派来了血妖，胡巴这次能否逃脱呢？', poster: 'cloud://mo-xiang-wx-yun.6d6f-mo-xiang-wx-yun-1301177803/source/video/poster/poster-捉妖记2-坏妖王来袭.png' }, 
       { id: 2, title: '生活大爆炸-父母对弈', type: '影视剪辑', playTime: '00:21', src: 'cloud://mo-xiang-wx-yun.6d6f-mo-xiang-wx-yun-1301177803/source/video/生活大爆炸-父母对弈.mp4', detail: '离异的莱纳德父母因为他的婚礼终于重逢，这对冤家会爆发怎样的战争呢？', poster: 'cloud://mo-xiang-wx-yun.6d6f-mo-xiang-wx-yun-1301177803/source/video/poster/poster-生活大爆炸-父母对弈.png' },
-      { id: 3, title: '当幸福来敲门-克里斯的一天', type: '影视剪辑', playTime: '00:21', src: 'cloud://mo-xiang-wx-yun.6d6f-mo-xiang-wx-yun-1301177803/source/video/当幸福来敲门-克里斯的一天.mp4', detail: '离异的莱纳德父母因为他的婚礼终于重逢，这对冤家会爆发怎样的战争呢？', poster: 'cloud://mo-xiang-wx-yun.6d6f-mo-xiang-wx-yun-1301177803/source/video/poster/poster-生活大爆炸-父母对弈.png' }
+      { id: 3, title: '当幸福来敲门-克里斯的一天', type: '影视剪辑', playTime: '00:21', src: 'cloud://mo-xiang-wx-yun.6d6f-mo-xiang-wx-yun-1301177803/source/video/当幸福来敲门-克里斯的一天.mp4', detail: '当我们感叹生活辛酸时，看看克里斯狗血却充满斗志的一天吧？', poster: 'cloud://mo-xiang-wx-yun.6d6f-mo-xiang-wx-yun-1301177803/source/video/poster/poster-当幸福来敲门-克里斯的一天.png' }
     ]
   },
 
